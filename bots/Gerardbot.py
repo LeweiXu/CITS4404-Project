5d61c547874ea207a795ff005190b4b17a87eccf
GVNewman <GVNewman@users.noreply.github.com>
2025-04-17 18:45:23 +0800

    Update Gerardbot.py

--- bots/Gerardbot.py
+++ bots/Gerardbot.py
@@ -1,18 +1,20 @@
 import Optimisers
 
-class bot:                                          # a general class that all bots use
-    def __init__(self,f):                           # f is a function of the hyperparameters and data that returns buy signals
-        self.n = f.__code__.co_argcount - 1         # number of hyperparameters, used by optimisation algorithm
-        self.f = f                                  
+class bot:                                                  # a general class that all bots use
+    def __init__(self,f,bounds):                            # f is a function of the hyperparameters and data that returns buy signals
+        self.n = f.__code__.co_argcount - 1                 # number of hyperparameters, used by optimisation algorithm
+        self.f = f       
+        self.bounds = bounds                           
 
     def buySignals(self, hyperParameters, data):
         return self.f(hyperParameters, data)
         
-    def TotalReturn(self, hyperParameters, data):
+    def TotalReturn(self, hyperParameters, data): #Calculates the total return given hyperparameters and data
+        signals = self.f(hyperParameters, data)
         pass
 
 
-def CompositeFilter(p1, p2, data):                                # a function defined by each person for their bot
+def CompositeFilter(p1, p2, data):                          # a function defined by each person for their bot, takes n hyper parameters and a data set, returns a list of buy signals
     prices = data['close'].values
 
     sma_low = wma(prices, p1, sma_filter(p1))
@@ -26,9 +28,9 @@ def CompositeFilter(p1, p2, data):                                # a function d
 
 
 
-GerardBot = bot(CompositeFilter) 
+GerardBot = bot(CompositeFilter, bounds) 
 
-OptimalParams = Optimisers.ABC(GerardBot, data)
+OptimalParams = Optimisers.ABC(GerardBot, data)             # Optimiser uses object.n to determine number of hyperparameters to optimise, bounds to determine their bounds and optimises based on the TotalReturn function
 
 
 Score = GerardBot.TotalReturn(OptimalParams, data)
